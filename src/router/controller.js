e155129f1b85667f259cd5a5e9eeb0d3cd30a87b
Hithesh-Devadiga <mr.hithesh.devadiga@gmail.com>
2021-01-23 13:51:35 +0530

    updated api

--- src/router/controller.js
+++ src/router/controller.js
@@ -6,4 +6,9 @@ router.get('/home', async (req, res) => {
    res.render('index')
 })
 
+router.post('/upload', async (req, res) => {
+   console.log(req.body)
+   res.status(200).json({ status: true, message: 'hi'})
+})
+
 module.exports = router
